7665e37a8f28d005418faf8fdece76b48cad249c
Topher <topher6345@gmail.com>
2018-01-12 14:24:44 -0800

    Update gb.cr

--- gb.cr
+++ gb.cr
@@ -1,96 +1,68 @@
 require "io/console"
 
 class String
-  def colorize(color)
-    case color
-    when :blue  then "\e[0;34;49m#{self}\e[0m"
-    when :green then "\e[0;32;49m#{self}\e[0m"
-    else
-      self
-    end
+  def blue
+    "\e[0;34;49m#{self}\e[0m"
+  end
+
+  def red
+    "\e[0;31;49m#{self}\e[0m"
+  end
+
+  def green
+    "\e[0;32;49m#{self}\e[0m"
   end
 
   def on_yellow
-    "\e[0;39;43m#{self}\e[0m"
-  end
-
-  def uncolorize
-    str = String.new
-    scan_for_colors.each do |match|
-      str = str + match[3]
-    end
-    str
-  end
-
-  def scan_for_colors
-    scan(/\033\[([0-9;]+)m(.+?)\033\[0m|([^\033]+)/m).map do |match|
-      split_colors(match)
-    end
-  end
-
-  def split_colors(match)
-    colors = (match[0] || "").split(";")
-    array = ["", "", "", ""]
-    array[0], array[1], array[2] = colors if colors.size == 3
-    # puts colors.class
-    array[1] = colors.first if colors.size == 1
-    array[3] = match[1] || match[2]
-    array
+    "\e[0;30;43m#{self}\e[0m"
   end
 end
 
-branches = `git branch  --sort=-committerdate | head -n 13 | sort`.split("\n")
-branch_count = branches.size
+branches = `git branch --sort=-committerdate | head -n 13 | sort`.split("\n") || ([] of String)
+commit_messages = branches.map { |b| `git show -s --format=%B #{b} | cat` }
 current_branch = `git branch`.split("\n").select { |s| s.match(/\*/) }.first[2..-1]
-current_position = branches.index { |s| s.match(/#{current_branch}/) }
+
+current_position = branches.index { |s| s.match(/#{current_branch}/) } || 0
 branches.map! { |o| o.size > 0 ? o[2..-1] : o }
-puts current_position
-# Mark current branch in blue
-exit(45) if current_position.nil?
-# branches[current_position] = branches[current_position].colorize(:blue)
-
 begin_position = current_position
+branch_count = branches.size
 
-def draw(current_position, branches, begin_position)
+def draw(current_position, branches, begin_position, commit_messages)
   system("clear")
-  return if branches.nil?
+  output = ""
   branches.each_with_index do |tail, index|
-    if (index == current_position) && begin_position == current_position
-      puts(("* " + tail).colorize(:blue))
-    elsif (index == current_position) && begin_position != current_position
-      puts(("* " + tail).colorize(:green))
-    else
-      puts "  " + tail
-    end
+    output = output + (
+      if begin_position == current_position && (index == current_position)
+        ("* " + tail).blue + "\n"
+      elsif begin_position != current_position && (index == current_position)
+        ("* " + tail).green + "\n"
+      else
+        "  " + tail + "\n"
+      end
+    )
   end
-  exit(1) if begin_position.nil?
-  exit(1) if current_position.nil?
-  puts "git branch " + branches[begin_position].dup.colorize(:blue) +
-       " → " + branches[current_position].dup.colorize(:green)
-  puts "\n"
-  puts branches[current_position]
-  commit_message = `git show -s --format=%B #{branches[current_position]} | cat`
-  puts commit_message.colorize(:black).on_yellow
+  puts output +
+       "\n" +
+       "git branch " +
+       branches[begin_position].blue +
+       " → ".red +
+       branches[current_position].green +
+       "\n" +
+       commit_messages[current_position].on_yellow
 end
 
-draw(current_position, branches, begin_position)
+draw(current_position, branches, begin_position, commit_messages)
+
 loop do
   case STDIN.raw(&.read_char)
-  when 'j'
-    exit(23) if current_position.nil?
-    current_position < branch_count - 1 ? (current_position += 1) : 0
-  when 'k'
-    exit(38) if current_position.nil?
-    current_position > 0 ? (current_position -= 1) : 0
-  when 'q'
-    exit(0)
-  when '\r'
-    break
+  when 'j'  then current_position < branch_count - 1 ? (current_position += 1) : 0
+  when 'k'  then current_position > 0 ? (current_position -= 1) : 0
+  when '\r' then break
   else
     exit(1)
   end
-  draw(current_position, branches, begin_position)
+  draw(current_position, branches, begin_position, commit_messages)
 end
 
-exit(1) if current_position.nil?
 puts `git checkout #{branches[current_position]}`
+    
