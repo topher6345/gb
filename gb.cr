d8582aaca9e00c124c6fefdbf2a9d48486dabaa7
Christopher Saunders <topher6345@gmail.com>
2018-02-03 20:44:44 -0800

    Fix regex and j bug

--- gb.cr
+++ gb.cr
@@ -20,7 +20,7 @@ branches = `git branch --sort=-committerdate | head -n 13 | sort`.split("\n") ||
 commit_messages = branches.map { |branch| `git show -s --format=%B #{branch} | cat` }
 current_branch = `git branch`.split("\n").select { |branch| branch.match(/\*/) }.first[2..-1]
 
-current_position = branches.index { |branch| branch.match(/#{current_branch}/) } || 0
+current_position = branches.index { |branch| branch.match(/\b#{current_branch}\b/) } || 0
 branches.map! { |branch| branch.size > 0 ? branch[2..-1] : branch }
 begin_position = current_position
 branch_count = branches.size
@@ -55,8 +55,10 @@ require "io/console" # if ruby
 loop do
   # case STDIN.getch # if ruby
   case STDIN.raw(&.read_char) # if crystal
-  when 'j' then current_position < branch_count - 1 ? (current_position += 1) : 0
-  when 'k' then current_position > 0 ? (current_position -= 1) : 0
+  when 'j'
+    current_position < (branch_count - 2) ? (current_position += 1) : 0
+  when 'k'
+    current_position > 0 ? (current_position -= 1) : 0
   when '\r'
     puts `git checkout #{branches[current_position]}`
     exit(0)
